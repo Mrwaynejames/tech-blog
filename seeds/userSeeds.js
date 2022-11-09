6ecff5e63710ffc05eb1e0bc6378fcaa2619d0ba
wayne james <jackmcwilliams@Jacks-MacBook-Air.local>
2022-11-08 21:53:12 -0800

    user seed

--- seeds/userSeeds.js
+++ seeds/userSeeds.js
@@ -1,1 +1,20 @@
 const { Comment } = require('../models');
+
+const userData = [
+    {
+        username: 'Jack',
+        password: 'Jwjm'
+    },
+    {
+        username: 'John',
+        password: 'Jjm'
+    },
+    {
+        username: 'Jill',
+        password: 'Jam'
+    }
+];
+
+const seedUsers = () => User.bulkCreate(userData);
+
+module.exports = seedUsers;
